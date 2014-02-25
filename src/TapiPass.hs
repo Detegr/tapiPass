5d1beb0f13ef94ef308882bc022b13518b3be12f
Antti Keränen <detegr@gmail.com>
2014-02-25 20:11:18 +0200

    Data.HashTable testing

--- src/TapiPass.hs
+++ src/TapiPass.hs
@@ -1,2 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import qualified Data.HashTable.ST.Basic as HT
+import Control.Monad.ST.Safe
+import Data.Text
+import Data.Maybe (fromMaybe)
+import qualified Data.Text.IO as TIO
+
+type TapiPassHT s = HT.HashTable s Text Text
+
+hashTest :: ST s Text
+hashTest = do
+  ht <- HT.new :: ST s (HT.HashTable s Text Text)
+  HT.insert ht "key" "val"
+  mb <- HT.lookup ht "key"
+  return $ fromMaybe "nothing" mb
+
 main :: IO()
-main = putStrLn "Hello tapiPass!"
+main = TIO.putStrLn $ runST hashTest
